093c296567bb1fa4995ef35ccd302119360dccd6
Anierin <anselme.revuz@Gmail.com>
2018-05-28 21:06:58 +0200

    fin bug liste vide

--- drive_module.py
+++ drive_module.py
@@ -56,7 +56,7 @@ def get_file(name,check=True):
     drive = auth_drive()
 
     file_want_list = drive.ListFile({'q': "trashed=false and title='" + name + "'"}).GetList()
-    print(len(file_want_list))
+    
     if len(file_want_list) == 0 :
         print(f'Failure : No file with this name')
         raise ValueError
@@ -80,6 +80,5 @@ def get_file(name,check=True):
                 print(f"file already get")
                 return # file already in, no download
     
-    print(len(file_want_list))
     file_want_list[0].GetContentFile(name)
     print(f"file downloaded  {addtxt}")
